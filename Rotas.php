b2b57c156562efd5679229e38e2a342a838b714e
Leandro Coutinho <leandrocoutinho.dev@gmail.com>
2024-07-24 01:10:49 -0300

    Update Rotas.php

--- Rotas.php
+++ Rotas.php
@@ -2,6 +2,8 @@
 $request = $_SERVER['REQUEST_URI'];
 $request = explode('?', $request)[0];
 if ($request == "/"){
-require __DIR__ . "Home.php";
-} 
+require __DIR__ . "index.php";
+} elseif($request == "/home"){
+  require __DIR__ . "Home.php";
+}
 ?>
