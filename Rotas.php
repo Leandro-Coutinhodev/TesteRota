43451c21a86b6cd269504acfa511492c241160f7
Leandro Coutinho <leandrocoutinho.dev@gmail.com>
2024-07-24 01:25:07 -0300

    Update Rotas.php

--- Rotas.php
+++ Rotas.php
@@ -1,9 +1,22 @@
 <?php
+// Obtenha a URL solicitada
 $request = $_SERVER['REQUEST_URI'];
+
+// Remova qualquer parâmetro de consulta da URL
 $request = explode('?', $request)[0];
-if ($request == "/"){
-require __DIR__ . "index.php";
-} elseif($request == "/home"){
-  require __DIR__ . "/Home.php";
+
+// Defina suas rotas
+switch ($request) {
+    case '/' :
+        require __DIR__ . '/index.php';
+        break;
+    case '/home' :
+        require __DIR__ . '/Home.php';
+        break;
+    default:
+        http_response_code(404);
+        //require __DIR__ . '/404.php';
+        break;
 }
+
 ?>
